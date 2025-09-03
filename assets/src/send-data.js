b61abd4a1b948903b24ba7dac34df0880dd99f79
Kamil <kamil.marek.pietruszka@gmail.com>
2025-09-03 23:18:32 +0200

    done-delete-basic-version

--- assets/src/send-data.js
+++ assets/src/send-data.js
@@ -11,13 +11,13 @@ class Ulubione {
 		document.addEventListener("DOMContentLoaded", () => {
 			document.querySelectorAll(".ulub").forEach((button) => {
 				button.addEventListener("click", (e) =>
-					this.clickUlubhendler(e)
+					this.clickUlubheHandller(e)
 				);
 			});
 		});
 	}
 
-	async clickUlubhendler(e) {
+	async clickUlubheHandller(e) {
 		e.preventDefault();
 
 		let currentClicked = e.target.closest("button");
@@ -41,35 +41,32 @@ class Ulubione {
 					),
 				}
 			);
-			currentClicked.setAttribute("");
 		} catch {
-			console.error("Coś poszło Mucho Grande nie tak");
+			console.error();
 		}
 	}
 
 	async deletePost(currentClicked) {
-		const favId = currentClicked.getAttribute("");
-		if (favId) {
-			currentClicked.setAttribute("data-ulubione-", favId);
-			console.log("Udało się wygenerować unikalne ID");
-		} else {
-			console.log("Nie wygenerowano nowegoID");
+		const originalID = Number(
+			currentClicked.getAttribute("data-original-post-id")
+		);
+		if (!Number.isFinite(originalID)) {
+			console.warn(
+				"Nie udało się ustawić akceptowalnej wartości, atrybut data musi być liczbą"
+			);
+			return;
 		}
 		try {
 			const response = await axios.delete(
 				localAddress + "/wp-json/ulub/v1/sendUlub",
 				{
 					data: {
-						list_type:
-							currentClicked.getAttribute("data-list-type"),
-						favorite_post_id:
-							currentClicked.getAttribute("favorite_post_id"),
+						original_post_id: originalID,
 					},
 				}
 			);
-		} catch (error) {
-			console.error(error);
-			console.warn("Brak data-ulubione-id – nie wiem co usunąć");
+		} catch {
+			console.error();
 		}
 	}
 }
--- include/ulubione-route.php
+++ include/ulubione-route.php
@@ -20,34 +20,91 @@ function ulubioneRoutes() {
 		)
 	);
 
-	function createPost( $data ) {
-		$original_post_id   = absint( $data->get_param( 'original_post_id' ) );
-		$post               = get_post( $original_post_id );
-		$favorite_list_type = absint( $data->get_param( 'list-type' ) );
-		$favorite_post_id   = wp_insert_post( [ 
-			'post_title' => $post->post_title,
-			'post_content' => $post->post_content,
-			'post_type' => 'ulubione',
-			'post_status' => 'publish',
-
-			'meta_input' => [ 
-				'favorite_post_id' => $original_post_id,
-				'favorite_list_type' => $favorite_list_type
-			]
-		] );
-		return [ 
-			'success' => true,
-			'favorite_post_id' => (int) $favorite_post_id,
-			'original_post_id' => (int) $original_post_id,
-			'list_type' => $favorite_list_type,
-		];
-	}
 
 
-	function deletePost( $data ) { { {
-			}
-			// todoDopisz funkcję, która usunie wp_delete post czy jakoś tak
-		}
-	}
 
 }
+
+function createPost( WP_REST_Request $request ) {
+	$original_post_id   = absint( $request->get_param( 'original_post_id' ) );
+	$post               = get_post( $original_post_id );
+	$favorite_list_type = absint( $request->get_param( 'list_type' ) );
+	$favorite_cpt_id    = wp_insert_post( [ 
+		'post_title' => $post->post_title,
+		'post_content' => $post->post_content,
+		'post_type' => 'ulubione',
+		'post_status' => 'publish',
+
+		'meta_input' => [ 
+			'favorite_post_id' => $original_post_id,
+			'favorite_list_type' => $favorite_list_type
+		]
+	] );
+	return new WP_REST_Response( [ 
+		'success' => true,
+		'favorite_cpt_id' => (int) $favorite_cpt_id,
+		'original_post_id' => (int) $original_post_id,
+		'list_type' => $favorite_list_type,
+
+	], 201 );
+}
+
+
+function deletePost( $request ) {
+	$original_post_id = absint( $request->get_param( 'original_post_id' ) );
+	// $favorite_list_type = absint( $request->get_param( 'list_type' ) );
+	if ( ! $original_post_id ) {
+		return new WP_Error( 'bad_request', 'Brak liczby w ID', [ 
+			'status' => 400
+
+		] );
+
+	}
+
+	if ( get_current_user_id() ) {
+		return new WP_Error( 'not_logged_in', 'Musisz być zalogowany', [ 
+			'status' => 401
+		] );
+	}
+
+	// TODOWP_Query START 
+	$finder_query = [ 
+		'post_type' => 'ulubione',
+		'posts_per_page' => 1,
+		'post_status' => 'publish',
+		'no_found_rows' => true,
+		'author' => get_current_user_id(),
+		'fields' => 'ids',
+		'meta_query' => [ [ 
+			'key' => 'favorite_post_id',
+			'value' => $original_post_id,
+			'compare' => '=',
+			'type' => 'NUMERIC',
+		],
+
+			// [ 
+			// 	'key' => 'favorite_list_type',
+			// 	'value' => $favorite_list_type,
+			// 	'compare' => '=',
+			// 	'type' => 'NUMERIC',
+			// ],
+
+		]
+	];
+	$query_result = new WP_Query( $finder_query );
+	$fav_ids      = $query_result->posts;
+	if ( ! empty( $fav_ids ) ) {
+		$fav_id = (int) $fav_ids[0];
+	} else {
+		$fav_id = 0;
+		return "Nie udało się pobrać ID ID wynosi 0";
+	}
+
+	// TODOWP_Query END 
+
+	// TODODelete FN START
+	wp_delete_post( $fav_id, true );
+	// TODODelete FN END 
+}
+
+
